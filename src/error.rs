b6f073a6e0d2ee44742263af3d8bba9732b0d3ea
Ras Bas <basgug25@gmail.com>
2026-06-06 19:12:15 +0200

    Add error handlers

--- src/error.rs
+++ src/error.rs
@@ -1,1 +1,14 @@
-pub struct Error;
+use thiserror::Error as ThisError;
+
+#[derive(Debug, ThisError)]
+pub enum Error {
+    #[error("io error: {0}")]
+    Io(#[from] std::io::Error),
+
+    #[error("toml serialize error: {0}")]
+    TomlSer(#[from] toml::ser::Error),
+
+    #[error("toml parse error: {0}")]
+    TomlDe(#[from] toml::de::Error),
+}
+
