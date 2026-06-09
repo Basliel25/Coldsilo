440b49da289d12b0122a161167c631a5f4eafb5d
Ras Bas <basgug25@gmail.com>
2026-06-09 15:56:28 +0200

    Added a hashing function a fixed file buffer

--- src/ops.rs
+++ src/ops.rs
@@ -7,7 +7,22 @@ use std::path::Path;
 use std::io::{Read, Write};
 use sha2::{Digest, Sha256};
 
-fn hash_file(path: &Path) -> Result<String, Error> {todo!()}
+fn hash_file(path: &Path) -> Result<String, Error> {
+
+    let mut file = File::open(path)?;
+    let mut hasher = Sha256::new();
+
+    let mut buf = [0u8; 65536];
+    loop {
+        let n = file.read(&mut buf)?;
+        if n == 0 {
+            break;
+        }
+        hasher.update(&buf[..n]);
+    }
+
+    Ok(format!("{:x}", hasher.finalize()))
+}
 
 pub fn offload(
     source: &Path, 
